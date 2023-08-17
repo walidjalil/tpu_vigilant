0d7c51403294aba5b3e32ae5200e2cd9780c62f1
Walid Abdul Jalil <walidajalil@MacBook-Pro-som-tillhor-Walid.local>
2023-08-18 01:16:58 +0200

    hej

--- multiTPU.py
+++ multiTPU.py
@@ -11,20 +11,30 @@ import torch_xla.distributed.parallel_loader as pl
 from torchvision import datasets
 
 # Define the CNN
-class Net(nn.Module):
+class ComplexCNN(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        super(ComplexCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
         self.dropout1 = nn.Dropout2d(0.25)
         self.dropout2 = nn.Dropout2d(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc1 = nn.Linear(128 * 3 * 3, 256)
+        self.fc2 = nn.Linear(256, 10)
 
     def forward(self, x):
         x = self.conv1(x)
+        x = self.bn1(x)
         x = F.relu(x)
         x = self.conv2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv3(x)
+        x = self.bn3(x)
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
         x = self.dropout1(x)
@@ -34,6 +44,7 @@ class Net(nn.Module):
         x = self.dropout2(x)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
 
 # Define train and test functions
 def train(model, device, train_loader, optimizer, epoch):
@@ -73,7 +84,7 @@ def test(model, device, test_loader):
 def _mp_fn(rank, flags):
     torch.manual_seed(flags['seed'])
     device = xm.xla_device()
-    model = Net().to(device)
+    model = ComplexCNN()().to(device)
     batch_size = flags['batch_size']
     test_batch_size = flags['test_batch_size']
     lr = flags['lr']
